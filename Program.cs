c918b262c48ec86932cea57a2ab5ec62259a1636
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-11 12:54:39 -0600

    feat: set up connection with database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using shifts.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ShiftsContext>(opt =>
+{
+  opt.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+    .EnableSensitiveDataLogging();
+  opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,12 +18,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -21,7 +31,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+  name: "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
