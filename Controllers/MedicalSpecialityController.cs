447293e2e9d5e0e355d42c297cdb4138dd635250
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-11 14:43:22 -0600

    feat: add functionality to edit a medical speciality

--- Controllers/MedicalSpecialityController.cs
+++ Controllers/MedicalSpecialityController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using shifts.Models;
 
 namespace shifts.Controllers;
@@ -15,5 +16,38 @@ public class MedicalSpecialityController : Controller
   public IActionResult Index()
   {
     return View(_context.MedicalSpecialities.ToList());
+  }
+
+  public IActionResult Edit(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var medicalSpeciality = _context.MedicalSpecialities.Find(id);
+    if (medicalSpeciality == null) return NotFound();
+
+    return View(medicalSpeciality);
+  }
+
+  [HttpPost]
+  public IActionResult Edit(int id, [Bind("Id, Description")] MedicalSpeciality medicalSpeciality)
+  {
+    if (id != medicalSpeciality.Id) return NotFound();
+
+    if (ModelState.IsValid)
+    {
+      try
+      {
+        _context.Update(medicalSpeciality);
+        _context.SaveChanges();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        throw new Exception($"MedicalSpeciality {id} not found!");
+      }
+
+      return RedirectToAction(nameof(Index));
+    }
+
+    return View(medicalSpeciality);
   }
 }
