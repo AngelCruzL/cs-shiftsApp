ac6ccc0711df6c1b80bff7173becd4332cc703d1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-11 19:15:33 -0600

    feat: override OnModelCreating function to add custom validation

--- Models/ShiftsContext.cs
+++ Models/ShiftsContext.cs
@@ -10,4 +10,16 @@ public class ShiftsContext : DbContext
   }
 
   public DbSet<MedicalSpeciality> MedicalSpecialities { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<MedicalSpeciality>(s =>
+      {
+        s.ToTable("MedicalSpecialities");
+        s.HasKey(s => s.Id);
+        s.Property(s => s.Id).ValueGeneratedOnAdd();
+        s.Property(s => s.Description).IsRequired().HasMaxLength(200).IsUnicode(false);
+      }
+    );
+  }
 }
