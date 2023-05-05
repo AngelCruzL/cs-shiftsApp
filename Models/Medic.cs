542b9dae01f07d5906e8d33db97dc8eb33123ea6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-04 21:52:46 -0600

    Add medic model

---
+++ Models/Medic.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace shifts.Models;
+
+public class Medic
+{
+  [Key] public int Id { get; set; }
+  [Display(Name = "Nombre(s)")] public string Name { get; set; }
+  [Display(Name = "Apellidos")] public string LastName { get; set; }
+  [Display(Name = "Dirección")] public string Address { get; set; }
+  [Display(Name = "Número Teléfonico")] public string PhoneNumber { get; set; }
+  [Display(Name = "Correo Electrónico")] public string Email { get; set; }
+  public DateTime ScheduleFrom { get; set; }
+  public DateTime ScheduleUntil { get; set; }
+}
