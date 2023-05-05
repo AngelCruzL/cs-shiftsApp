a9e2477556eee48e2dc2fa4e75aaadee906aab9d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-04 22:30:41 -0600

    Add medic controller and views scaffold

--- Models/ShiftsContext.cs
+++ Models/ShiftsContext.cs
@@ -11,6 +11,8 @@ public class ShiftsContext : DbContext
 
   public DbSet<MedicalSpeciality> MedicalSpecialities { get; set; }
   public DbSet<Patient> Patients { get; set; }
+  public DbSet<Medic> Medic { get; set; }
+
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
@@ -26,29 +28,28 @@ public class ShiftsContext : DbContext
     modelBuilder.Entity<Patient>(entity =>
     {
       entity.ToTable("Patients");
-      
+
       entity.HasKey(e => e.Id);
-      
+
       entity.Property(p => p.Name)
         .IsRequired()
         .HasMaxLength(50)
         .IsUnicode(false);
-      
+
       entity.Property(p => p.LastName)
-        .IsRequired().
-        HasMaxLength(50)
+        .IsRequired().HasMaxLength(50)
         .IsUnicode(false);
-      
+
       entity.Property(p => p.Address)
         .IsRequired()
         .HasMaxLength(250)
         .IsUnicode(false);
-      
+
       entity.Property(p => p.PhoneNumber)
         .IsRequired()
         .HasMaxLength(20)
         .IsUnicode(false);
-      
+
       entity.Property(p => p.Email)
         .IsRequired()
         .HasMaxLength(100)
