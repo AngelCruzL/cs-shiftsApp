675204f54698f7b5016f3ffd7444f7f9f3dbcfa1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-04 21:15:39 -0600

    Add create new patient view

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -6,9 +6,9 @@ public class Patient
 {
   [Key] public int Id { get; set; }
 
-  [Display(Name = "Nombre")] public string Name { get; set; }
+  [Display(Name = "Nombre(s)")] public string Name { get; set; }
 
-  [Display(Name = "Apellido")] public string LastName { get; set; }
+  [Display(Name = "Apellidos")] public string LastName { get; set; }
   [Display(Name = "Dirección")] public string Address { get; set; }
   [Display(Name = "Número Teléfonico")] public string PhoneNumber { get; set; }
   [Display(Name = "Correo Electrónico")] public string Email { get; set; }
