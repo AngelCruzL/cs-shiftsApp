4d5b261b97e60159517ff82cca7f5e645f3af0d6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-04 20:07:06 -0600

    Add Patients migration

--- Migrations/20230505013527_PatientMigration.Designer.cs
+++ Migrations/20230505013527_PatientMigration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using shifts.Models;
 
@@ -10,9 +11,11 @@ using shifts.Models;
 namespace shifts.Migrations
 {
     [DbContext(typeof(ShiftsContext))]
-    partial class ShiftsContextModelSnapshot : ModelSnapshot
+    [Migration("20230505013527_PatientMigration")]
+    partial class PatientMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230505013527_PatientMigration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shifts.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "MedicalSpecialities",
+                type: "varchar(200)",
+                unicode: false,
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "MedicalSpecialities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldUnicode: false,
+                oldMaxLength: 200);
+        }
+    }
+}
