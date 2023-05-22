a67035ff7920c681887451888acce87af75078cd
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-22 17:26:00 -0600

    Add data annotations to Medic model

--- Models/Medic.cs
+++ Models/Medic.cs
@@ -5,12 +5,38 @@ namespace shifts.Models;
 public class Medic
 {
   [Key] public int Id { get; set; }
-  [Display(Name = "Nombre(s)")] public string Name { get; set; }
-  [Display(Name = "Apellidos")] public string LastName { get; set; }
-  [Display(Name = "Dirección")] public string Address { get; set; }
-  [Display(Name = "Número Teléfonico")] public string PhoneNumber { get; set; }
-  [Display(Name = "Correo Electrónico")] public string Email { get; set; }
-  [Display(Name = "Hora de Inicio")] public DateTime ScheduleFrom { get; set; }
-  [Display(Name = "Hora de Salida")] public DateTime ScheduleUntil { get; set; }
+
+  [Required(ErrorMessage = "Debe ingresar al menos un nombre")]
+  [Display(Name = "Nombre(s)")]
+  public string Name { get; set; }
+
+  [Required(ErrorMessage = "Debe ingresar al menos un apellido")]
+  [Display(Name = "Apellidos")]
+  public string LastName { get; set; }
+
+  [Required(ErrorMessage = "Debe ingresar una dirección")]
+  [Display(Name = "Dirección")]
+  public string Address { get; set; }
+
+  [Required(ErrorMessage = "Debe ingresar un número teléfonico")]
+  [Display(Name = "Número Teléfonico")]
+  public string PhoneNumber { get; set; }
+
+  [Required(ErrorMessage = "Debe ingresar un correo electrónico")]
+  [Display(Name = "Correo Electrónico")]
+  [EmailAddress(ErrorMessage = "Debe ingresar un correo electrónico válido")]
+  public string Email { get; set; }
+
+
+  [Display(Name = "Hora de Inicio")]
+  [DataType(DataType.Time)]
+  [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
+  public DateTime ScheduleFrom { get; set; }
+
+  [Display(Name = "Hora de Salida")]
+  [DataType(DataType.Time)]
+  [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
+  public DateTime ScheduleUntil { get; set; }
+
   public List<MedicMedicalSpeciality>? MedicMedicalSpecialities { get; set; }
 }
