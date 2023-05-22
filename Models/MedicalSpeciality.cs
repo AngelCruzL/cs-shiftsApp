f79d1a6c26a2093d529bfa8b0d0fd28b0def0190
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-22 17:13:13 -0600

    Add data annotations to MedicalSpeciality model

--- Models/MedicalSpeciality.cs
+++ Models/MedicalSpeciality.cs
@@ -6,6 +6,10 @@ public class MedicalSpeciality
 {
   [Key] public int Id { get; set; }
 
-  [Display(Name = "Descripción")] public string Description { get; set; }
+  [StringLength(200, ErrorMessage = "La descripción no puede tener más de 200 caracteres")]
+  [Required(ErrorMessage = "Debe ingresar una descripción")]
+  [Display(Name = "Descripción", Prompt = "Ingrese una descripción")]
+  public string Description { get; set; }
+
   public List<MedicMedicalSpeciality>? MedicMedicalSpecialities { get; set; }
 }
